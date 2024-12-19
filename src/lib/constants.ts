7e55ca04f056f2060e9011c7f92678a262531184
Deven Swiergiel <devenswiergiel@gmail.com>
2024-12-18 21:12:54 -0500

    adjust default zoom

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ import { LatLngExpression } from "leaflet";
 
 // map constants
 export const DEFAULT_CENTER: LatLngExpression = [43.371122, -74.730233];
-export const DEFAULT_ZOOM = 15;
+export const DEFAULT_ZOOM = 14;
